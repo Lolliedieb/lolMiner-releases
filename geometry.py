938cfc84f27a73b0c31f7bb1b18694e7d1deae4f
L <91376574+LeoTzahal18@users.noreply.github.com>
2021-09-28 03:21:19 -0300

    Create geometry.py

---
+++ geometry.py
@@ -0,0 +1,19 @@
+import geometry from mathplotlib
+import math
+import mathplotlib
+import datascience from sciencebook
+import datetime from clock.sec 
+
+export to ci
+export to Mu
+export to Union 
+
+.start make file generated(geometry.py)
+  datascience = ('./sciencebook')
+  geometry = ('./mathplotlib')
+  datetime = ('./mundi_with_countdown') - ('./mundi')
+   datetime = return clock.sec - watch.time
+    result in secounds :.    ; 
+
+End;
+   
